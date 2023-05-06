723e643b1f90befb2107d9020c47d2179a418432
David1234567l <35432315+David1234567l@users.noreply.github.com>
2023-05-06 13:40:24 +0000

    Pending changes exported from your codespace

---
+++ src/helper/paymentToken.ts
@@ -0,0 +1,15 @@
+import { Address } from '@graphprotocol/graph-ts';
+import { PaymentToken } from '../../generated/schema';
+import GlobalConstants from '../utils';
+
+export function getOrCreatePaymentToken(tokenAddress: Address): PaymentToken {
+  let token = PaymentToken.load(tokenAddress.toHexString());
+  if (!token) {
+    token = new PaymentToken(tokenAddress.toHexString());
+    //  token.symbol = PaymentTokens.get(tokenAddress.toHexString());
+    token.address = tokenAddress.toHexString();
+    token.numberOfSales = GlobalConstants.BI_ZERO;
+  }
+  return token as PaymentToken;
+}
+ 
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,20 +1,96 @@
-import { BigInt } from "@graphprotocol/graph-ts";
- import { Transfer } from "../generated/NFTContract/NFTContract";
- import { Token, Transfer as TransferEntity } from "../generated/schema";
- export function handleTransfer(event: Transfer): void {
- let token = Token.load(event.params.tokenId.toString());
- if (token == null) {
- token = new Token(event.params.tokenId.toString());
- token.metadataURI = "";
- }
- token.owner = event.params.to;
- token.save();
- let transfer = new TransferEntity(
- event.transaction.hash.toHex() + "-" + event.logIndex.toString()
- );
- transfer.token = token.id;
- transfer.from = event.params.from;
- transfer.to = event.params.to;
- transfer.timestamp = event.block.timestamp;
- transfer.save();
- }
+import { ethereum } from "@graphprotocol/graph-ts";
+import {
+  OrdersMatched,
+  OrderApprovedPartOne,
+  OrderApprovedPartTwo,
+} from "../generated/OpenSea/OpenSea";
+import {
+  getOrCreateAccount,
+  updateBuyerAggregates,
+  updateSellerAggregates,
+} from "./helper/account";
+import { getOrCreateAuction } from "./helper/auction";
+import {
+  getOrCreateCollection,
+  updateCollectionAggregates,
+} from "./helper/collection";
+import { getOrCreateFee } from "./helper/fee";
+
+import { getOrCreateNft, updateNftMetrics } from "./helper/nft";
+import { getOrCreateSale, updateSale } from "./helper/sale";
+import { getOrCreatePaymentToken } from "./helper/paymentToken";
+import GlobalConstants from "./utils";
+import { getOrCreateContract } from "./helper/contract";
+
+export function handleOrdersMatched(event: OrdersMatched): void {
+  let buyHash = event.params.buyHash;
+  let sellHash = event.params.sellHash;
+  let price = GlobalConstants.convertPriceToBigDecimal(event.params.price);
+  let maker = event.params.maker;
+  let taker = event.params.taker;
+
+  let receipt = event.receipt;
+
+  if (receipt) {
+    for (let index = 0; index < receipt.logs.length; index++) {
+      const _topic0 = receipt.logs[index].topics[0];
+      const _address = receipt.logs[index].address;
+      if (
+        _topic0.equals(GlobalConstants.TRANSFER_SIG) &&
+        _address.toHexString() == GlobalConstants.GALAKTIC_GANG
+      ) {
+        const _tokenID = receipt.logs[index].topics[3];
+        const tokenId = ethereum.decode("uint256", _tokenID)!.toBigInt();
+
+        let buyer = getOrCreateAccount(maker);
+        let seller = getOrCreateAccount(taker);
+        let collection = getOrCreateCollection(_address.toHexString());
+        let nft = getOrCreateNft(tokenId, collection, maker);
+        let sale = getOrCreateSale(event);
+
+        updateCollectionAggregates(collection, buyer, price, nft);
+        updateNftMetrics(buyer, sale, tokenId, collection, nft);
+        updateSellerAggregates(seller, price);
+        updateBuyerAggregates(buyer, price);
+        updateSale(sale, buyHash, sellHash, buyer, seller, price, collection);
+
+        buyer.save();
+        seller.save();
+        collection.save();
+        nft.save();
+        sale.save();
+      }
+    }
+  }
+}
+
+export function handleOrderApprovedPartOne(event: OrderApprovedPartOne): void {
+  let fee = getOrCreateFee(event);
+  let collection = getOrCreateCollection(event.params.feeRecipient.toHexString());
+
+  fee.feeRecipient = collection.id;
+  fee.takerProtocolFee = event.params.takerProtocolFee;
+  fee.makerProtocolFee = event.params.makerProtocolFee;
+  fee.makerRelayerFee = event.params.makerRelayerFee;
+  fee.takerProtocolFee = event.params.takerRelayerFee;
+
+  fee.save();
+}
+
+export function handleOrderApprovedPartTwo(event: OrderApprovedPartTwo): void {
+  let auction = getOrCreateAuction(event.params.hash.toHexString(), event);
+  let paymentToken = getOrCreatePaymentToken(event.params.paymentToken);
+
+  auction.listingTime = event.params.listingTime;
+  auction.basePrice = event.params.basePrice;
+  auction.expirationTime = event.params.expirationTime;
+  auction.paymentToken = paymentToken.id;
+  auction.staticExtraData = event.params.staticExtradata;
+  auction.extra = event.params.extra;
+  auction.hash = event.params.hash;
+  auction.orderbook = event.params.orderbookInclusionDesired;
+
+  auction.save();
+  paymentToken.save();
+}
+
