6b49ad9a7bca0a3412493671af42fe0957913ac4
David1234567l <35432315+David1234567l@users.noreply.github.com>
2023-05-08 05:34:56 +0000

    Done

--- README.md
+++ README.md
@@ -1,2 +1,120 @@
-# OpenSea
-OpenSea is the world's first and largest NFT marketplace
+GraphQL Subgraph For OpenSea
+This subgraph is for OpenSea, a popular decentralized marketplace for NFTs. I will provide a
+general explanation of the schema, mapping file, and use cases for a subgraph that indexes data
+from the OpenSea marketplace, as well as provide test cases for the subgraph.
+Schema:
+The schema is defined in the schema.graphql file and represents the structure of the data you
+want to index and query. For the OpenSea subgraph, the schema includes the types Collection,
+Token, Sale, and Fee. These types represent entities related to NFT collections, individual
+tokens, sales transactions, and fees paid to the platform.
+Mapping file:
+The mapping file, usually named mapping.ts, contains the logic for processing and indexing data
+from the smart contracts on the blockchain. It maps events emitted by the contracts to the entities
+defined in the schema, and updates the data accordingly. For the OpenSea subgraph, mappings
+are defined for events such as new NFT listings, token transfers, or sale transactions.
+Use cases:
+With this OpenSea subgraph, developers can build various applications or features that leverage
+the indexed data. Some use cases include:
+1. Displaying historical sales data for a specific NFT or collection.
+2. Analyzing the most active buyers and sellers in the marketplace.
+3. Tracking the total volume of sales or fees paid on the platform.
+4. Identifying trends in NFT prices and activity over time.
+5. Providing personalized recommendations based on users' browsing and purchase history.
+Test cases:
+To test the OpenSea subgraph, after running yarn codegen and yarn build and deploying
+the subgraph on the graph, multiple GraphQL queries on ERC-721 tokens were tested to fetch
+the data indexed by the subgraph. To run these queries, you can use the [Graph
+Explorer](https://thegraph.com/explorer/) or a GraphQL client such as [Apollo
+Client](https://www.apollographql.com/docs/react/).
+Here are some example queries:
+1. Retrieve all purchases made by a specific account:
+{
+account(id: "0xdac904a2165a9439dc5f04b82ae762707d3bf4cf") {
+sales {
+id
+buyer {
+id
+}
+seller {
+id
+}
+price
+paymentToken {
+id
+}
+nft {
+id
+}
+timestamp
+}
+}
+}
+2. Query the first 10 collections, This query will return the first 10 NFTs with their owner and
+collection information.
+{
+nfts(first: 10) {
+id
+owner {
+id
+numberOfSales
+numberOfPurchases
+}
+collection {
+id
+nft {
+id
+symbol
+}
+}
+}
+}
+3. Query a specific token by its ID, including the token owner and related collection:
+{
+nft(id: "0xf4cd7e65348deb24e30dedee639c4936ae38b763-1023") {
+id
+owner {
+id
+numberOfSales
+numberOfPurchases
+}
+collection {
+id
+nft {
+id
+symbol
+}
+}
+}
+}
+4. Retrieve all sales for a specific collection:
+{
+collection(id: "0xf4cd7e65348deb24e30dedee639c4936ae38b763") {
+sales {
+id
+buyer {
+id
+}
+seller {
+id
+}
+price
+paymentToken {
+id
+}
+nft {
+id
+}
+timestamp
+}
+}
+}
+The reason that only ERC-721 NFTs work for this schema is that, although OpenSea also
+supports ERC-1155 NFTs, these tokens have a different structure and require a different schema
+to properly index and query them.
+In the current schema, the `Collection` type includes a `totalSupply` field, which is specific to
+ERC-721 tokens since they are indivisible and can only have a single token ID. On the other
+hand, ERC-1155 tokens can have multiple token IDs and different amounts for each ID, which
+makes the `totalSupply` field not applicable.
+Therefore, if you wanted to index ERC-1155 NFTs in the OpenSea marketplace, you would need
+to create a new schema specifically designed for them, taking into account their unique
+properties and structures.
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,102 +1,96 @@
-import { ethereum, log } from '@graphprotocol/graph-ts'
+import { ethereum } from "@graphprotocol/graph-ts";
 import {
-	OpenSea,
-	OrdersMatched,
-	OrderApprovedPartOne,
-	OrderApprovedPartTwo,
-} from '../generated/OpenSea/OpenSea'
+  OrdersMatched,
+  OrderApprovedPartOne,
+  OrderApprovedPartTwo,
+} from "../generated/OpenSea/OpenSea";
 import {
-	getOrCreateAccount,
-	updateBuyerAggregates,
-	updateSellerAggregates,
-} from './helper/accountHelper'
-import { getOrCreateAuction } from './helper/auctionHelper'
+  getOrCreateAccount,
+  updateBuyerAggregates,
+  updateSellerAggregates,
+} from "./functions/a1";
+import { getOrCreateAuction } from "./functions/a2";
 import {
-	getOrCreateCollection,
-	updateCollectionAggregates,
-} from './helper/collectionHelper'
-import { getOrCreateFee } from './helper/feeHelper'
+  getOrCreateCollection,
+  updateCollectionAggregates,
+} from "./functions/c";
+import { getOrCreateFee } from "./functions/f";
 
-import { getOrCreateNft, updateNftMetrics } from './helper/nftHelper'
-import { getOrCreateSale, updateSale } from './helper/saleHelper'
-import { getOrCreatePaymentToken } from './helper/paymentTokenHelper'
-import GlobalConstants from './utils'
-import { getOrCreateContract } from './helper/contractHelper'
+import { getOrCreateNft, updateNftMetrics } from "./functions/nft";
+import { getOrCreateSale, updateSale } from "./functions/sale";
+import { getOrCreatePaymentToken } from "./functions/p";
+import GlobalConstants from "./utils";
 
-//set of 3: REGULAR TRANSFER: https://etherscan.io/tx/0x9660bd19edec4f443068094d3ee9cf2c9b78fbc4a7888bb1a98d154a98041d0a#eventlog
 
 export function handleOrdersMatched(event: OrdersMatched): void {
-	let buyHash = event.params.buyHash
-	let sellHash = event.params.sellHash
-	let price = GlobalConstants.convertPriceToBigDecimal(event.params.price)
-	let maker = event.params.maker
-	let taker = event.params.taker
+  let buyHash = event.params.buyHash;
+  let sellHash = event.params.sellHash;
+  let price = GlobalConstants.convertPriceToBigDecimal(event.params.price);
+  let maker = event.params.maker;
+  let taker = event.params.taker;
 
-	let receipt = event.receipt
+  let receipt = event.receipt;
 
-	if (receipt) {
-		for (let index = 0; index < receipt.logs.length; index++) {
-			const _topic0 = receipt.logs[index].topics[0]
-			const _address = receipt.logs[index].address
-			if (
-				_topic0.equals(GlobalConstants.TRANSFER_SIG) &&
-				_address.toHexString() == GlobalConstants.GALAKTIC_GANG
-			) {
-				const _tokenID = receipt.logs[index].topics[3]
-				const tokenId = ethereum.decode('uin256', _tokenID)!.toBigInt()
+  if (receipt) {
+    for (let index = 0; index < receipt.logs.length; index++) {
+      const _topic0 = receipt.logs[index].topics[0];
+      const _address = receipt.logs[index].address;
+      if (
+        _topic0.equals(GlobalConstants.TRANSFER_SIG) &&
+        _address.toHexString() == GlobalConstants.GALAKTIC_GANG
+      ) {
+        const _tokenID = receipt.logs[index].topics[3];
+        const tokenId = ethereum.decode("uint256", _tokenID)!.toBigInt();
 
-				let buyer = getOrCreateAccount(maker)
-				let seller = getOrCreateAccount(taker)
-				let collection = getOrCreateCollection(_address.toHexString())
-				let nft = getOrCreateNft(tokenId, collection, maker)
-				let sale = getOrCreateSale(event)
-				//let openSeaContract = getOrCreateContract()
+        let buyer = getOrCreateAccount(maker);
+        let seller = getOrCreateAccount(taker);
+        let collection = getOrCreateCollection(_address.toHexString());
+        let nft = getOrCreateNft(tokenId, collection, maker);
+        let sale = getOrCreateSale(event);
 
-				updateCollectionAggregates(collection, buyer, price, nft)
-				updateNftMetrics(buyer, sale, tokenId, collection, nft)
-				updateSellerAggregates(seller, price)
-				updateBuyerAggregates(buyer, price)
-				updateSale(sale, buyHash, sellHash, buyer, seller, price, collection)
+        updateCollectionAggregates(collection, buyer, price, nft);
+        updateNftMetrics(buyer, sale, tokenId, collection, nft);
+        updateSellerAggregates(seller, price);
+        updateBuyerAggregates(buyer, price);
+        updateSale(sale, buyHash, sellHash, buyer, seller, price, collection);
 
-				buyer.save()
-				seller.save()
-				collection.save()
-				nft.save()
-				sale.save()
-			}
-		}
-	}
+        buyer.save();
+        seller.save();
+        collection.save();
+        nft.save();
+        sale.save();
+      }
+    }
+  }
 }
-//}
 
 export function handleOrderApprovedPartOne(event: OrderApprovedPartOne): void {
-	let fee = getOrCreateFee(event)
-	let collection = getOrCreateCollection(
-		event.params.feeRecipient.toHexString()
-	)
+  let fee = getOrCreateFee(event);
+  let collection = getOrCreateCollection(event.params.feeRecipient.toHexString());
 
-	fee.feeRecipient = collection.id
-	fee.takerProtocolFee = event.params.takerProtocolFee
-	fee.makerProtocolFee = event.params.makerProtocolFee
-	fee.makerRelayerFee = event.params.makerRelayerFee
-	fee.takerProtocolFee = event.params.takerRelayerFee
+  fee.feeRecipient = collection.id;
+  fee.takerProtocolFee = event.params.takerProtocolFee;
+  fee.makerProtocolFee = event.params.makerProtocolFee;
+  fee.makerRelayerFee = event.params.makerRelayerFee;
+  fee.takerProtocolFee = event.params.takerRelayerFee;
 
-	fee.save()
+  fee.save();
 }
 
 export function handleOrderApprovedPartTwo(event: OrderApprovedPartTwo): void {
-	let auction = getOrCreateAuction(event.params.hash.toHexString(), event)
-	let paymentToken = getOrCreatePaymentToken(event.params.paymentToken)
+  let auction = getOrCreateAuction(event.params.hash.toHexString(), event);
+  let paymentToken = getOrCreatePaymentToken(event.params.paymentToken);
 
-	auction.listingTime = event.params.listingTime
-	auction.basePrice = event.params.basePrice
-	auction.expirationTime = event.params.expirationTime
-	auction.paymentToken = paymentToken.id
-	auction.staticExtraData = event.params.staticExtradata
-	auction.extra = event.params.extra
-	auction.hash = event.params.hash
-	auction.orderbook = event.params.orderbookInclusionDesired
+  auction.listingTime = event.params.listingTime;
+  auction.basePrice = event.params.basePrice;
+  auction.expirationTime = event.params.expirationTime;
+  auction.paymentToken = paymentToken.id;
+  auction.staticExtraData = event.params.staticExtradata;
+  auction.extra = event.params.extra;
+  auction.hash = event.params.hash;
+  auction.orderbook = event.params.orderbookInclusionDesired;
 
-	auction.save()
-	paymentToken.save()
+  auction.save();
+  paymentToken.save();
 }
+
